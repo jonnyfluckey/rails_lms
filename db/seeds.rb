354ae452f162de4118f1ce22b5f8ad63df9d3772
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-03-05 18:52:43 -0700

    Finished LMS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+roles = ['teacher', 'ta', 'student']
+
+10.times do
+  course = Course.create(course_name: Faker::ChuckNorris.fact)
+
+
+  
+  10.times do
+    user= User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+    
+  
+
+      enrollment = Enrollment.create(role: roles.sample, course_id: course.id, user_id: user.id)
+
+  end
+
+end
+
+puts "All the data is seeded!"
